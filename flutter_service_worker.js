ebcb62ff7bcb0f196cc8db1c58ec9b5f4d29aa5d
RohanNaiduGetto <rohannaidu@gettoindia.com>
2025-08-06 15:06:13 +0530

    commit for ui changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -218,14 +218,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a77612b71817c563fff6b7504b5ed3c8
 "favicon.png": "60b424a2f695a4d923db533154d0b91e",
 "firebase-messaging-sw.js": "8c22821a9b40d5d4e5da60ce025c8cc8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "731c3f1b9e036f2c7b8059165599cd70",
+"flutter_bootstrap.js": "560ec8df7c86999b2239c1ed908ca030",
 "icons/Icon-192.png": "d5af74df5a4a4f9f145efa4813399a04",
 "icons/Icon-512.png": "5058e9e700edb2801cb86623c7559140",
 "icons/Icon-maskable-192.png": "d5af74df5a4a4f9f145efa4813399a04",
 "icons/Icon-maskable-512.png": "5058e9e700edb2801cb86623c7559140",
 "index.html": "3cc137f4589165adf9a402daef6d9a64",
 "/": "3cc137f4589165adf9a402daef6d9a64",
-"main.dart.js": "09ae51e7ec7f13e02acac7b1cde7f6ad",
+"main.dart.js": "eb790d3f834770985981f92042280fcb",
 "manifest.json": "85bca26cc80b8fe49a76867469198ad2",
 "version.json": "eb163e90fe2ddb320914c1e7fa6e2d70"};
 // The application shell files that are downloaded before a service worker can
