1f974d3e4f9355af9f494835fcf80cf16709e200
RohanNaiduGetto <rohannaidu@gettoindia.com>
2025-09-25 14:53:39 +0530

    Commit for merging the code.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1732450941"
+    serviceWorkerVersion: "2743777700"
   }
 });
